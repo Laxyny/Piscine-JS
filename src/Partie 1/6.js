b4090c16527d863f2a9ee4f12697e78ecd04550c
Laxyny <kevgreg30@gmail.com>
2026-01-21 10:00:03 +0100

    *ADD* Ajout 2 premières parties

---
+++ src/Partie 1/6.js
@@ -0,0 +1,7 @@
+/* 
+Maximum de 3 nombres
+Créez une fonction max3 qui trouve le plus grand parmi trois nombres.
+
+Contrainte
+Vous devez utiliser l'opérateur logique ET (&&) pour vérifier qu'un nombre est plus grand que les deux autres.
+*/
